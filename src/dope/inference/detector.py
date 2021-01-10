b25573a37435c49128f8cadf4730459278fbec2e
Blaine Miller <blaine141@gmail.com>
2021-01-10 16:51:30 -0500

    Fixing test and adding python3 warning

--- src/dope/inference/detector.py
+++ src/dope/inference/detector.py
@@ -239,7 +239,7 @@ class ObjectDetector(object):
     '''This class contains methods for object detection'''
 
     @staticmethod
-    def detect_object_in_image(net_model, pnp_solver, in_img, config):
+    def detect_object_in_image(net_model, pnp_solver, in_img, config, grid_belief_debug=False, norm_belief=True, run_sampling=False, network='dope'):
         '''Detect objects in a image using a specific trained network model'''
 
         if in_img is None:
@@ -255,7 +255,158 @@ class ObjectDetector(object):
         # Find objects from network output
         detected_objects = ObjectDetector.find_object_poses(vertex2, aff, pnp_solver, config)
 
-        return detected_objects
+        # Find objects from network output
+        detected_objects = ObjectDetector.find_object_poses(vertex2, aff, pnp_solver, config
+            # run_sampling=run_sampling,
+            # scale_factor = scale_factor,
+            # OFFSET_DUE_TO_UPSAMPLING = OFFSET_DUE_TO_UPSAMPLING
+            )
+
+        if not grid_belief_debug: 
+            return detected_objects, None
+        else:
+            # Run the belief maps debug display on the beliefmaps
+            upsampling = nn.UpsamplingNearest2d(scale_factor=1)
+            tensor = vertex2 # shape [9, 50, 50]
+            belief_imgs = []
+            in_img = (torch.tensor(in_img).float()/255.0)
+            in_img *= 0.7            
+
+            for j in range(tensor.size()[0]):
+                belief = tensor[j].clone()
+                if norm_belief:
+                    belief -= float(torch.min(belief).item())
+                    belief /= float(torch.max(belief).item())
+
+                # print (image_torch.size())
+                # raise()    
+                # belief *= 0.5
+                # print(in_img.size())
+                belief = upsampling(belief.unsqueeze(0).unsqueeze(0)).squeeze().squeeze().data 
+                belief = torch.clamp(belief, 0, 1).cpu()  
+                belief = torch.cat([
+                            # belief.unsqueeze(0) + in_img[:,:,0],
+                            # belief.unsqueeze(0) + in_img[:,:,1],
+                            # belief.unsqueeze(0) + in_img[:,:,2]
+                            belief.unsqueeze(0),
+                            belief.unsqueeze(0),
+                            belief.unsqueeze(0)
+                            ]).unsqueeze(0)
+                belief = torch.clamp(belief, 0, 1) 
+
+                # belief_imgs.append(belief.data.squeeze().cpu().numpy().transpose(1,2,0))
+                belief_imgs.append(belief.data.squeeze().numpy())
+
+            # Create the image grid
+            belief_imgs = torch.tensor(np.array(belief_imgs))
+
+            im_belief = ObjectDetector.get_image_grid(belief_imgs, None, mean=0, std=1)
+
+            return detected_objects, im_belief
+
+    
+    @staticmethod
+    def make_grid(tensor, nrow=8, padding=2,
+                  normalize=False, range_=None, scale_each=False, pad_value=0):
+        """Make a grid of images.
+        Args:
+            tensor (Tensor or list): 4D mini-batch Tensor of shape (B x C x H x W)
+                or a list of images all of the same size.
+            nrow (int, optional): Number of images displayed in each row of the grid.
+                The Final grid size is (B / nrow, nrow). Default is 8.
+            padding (int, optional): amount of padding. Default is 2.
+            normalize (bool, optional): If True, shift the image to the range (0, 1),
+                by subtracting the minimum and dividing by the maximum pixel value.
+            range (tuple, optional): tuple (min, max) where min and max are numbers,
+                then these numbers are used to normalize the image. By default, min and max
+                are computed from the tensor.
+            scale_each (bool, optional): If True, scale each image in the batch of
+                images separately rather than the (min, max) over all images.
+            pad_value (float, optional): Value for the padded pixels.
+        Example:
+            See this notebook `here <https://gist.github.com/anonymous/bf16430f7750c023141c562f3e9f2a91>`_
+        """
+        import math
+
+        if not (torch.is_tensor(tensor) or
+                (isinstance(tensor, list) and all(torch.is_tensor(t) for t in tensor))):
+            raise TypeError('tensor or list of tensors expected, got {}'.format(type(tensor)))
+
+        # if list of tensors, convert to a 4D mini-batch Tensor
+        if isinstance(tensor, list):
+            tensor = torch.stack(tensor, dim=0)
+
+        if tensor.dim() == 2:  # single image H x W
+            tensor = tensor.view(1, tensor.size(0), tensor.size(1))
+        if tensor.dim() == 3:  # single image
+            if tensor.size(0) == 1:  # if single-channel, convert to 3-channel
+                tensor = torch.cat((tensor, tensor, tensor), 0)
+            tensor = tensor.view(1, tensor.size(0), tensor.size(1), tensor.size(2))
+
+        if tensor.dim() == 4 and tensor.size(1) == 1:  # single-channel images
+            tensor = torch.cat((tensor, tensor, tensor), 1)
+
+        if normalize is True:
+            tensor = tensor.clone()  # avoid modifying tensor in-place
+            if range_ is not None:
+                assert isinstance(range_, tuple), \
+                    "range has to be a tuple (min, max) if specified. min and max are numbers"
+
+            def norm_ip(img, min, max):
+                img.clamp_(min=min, max=max)
+                img.add_(-min).div_(max - min + 1e-5)
+
+            def norm_range(t, range_):
+                if range_ is not None:
+                    norm_ip(t, range_[0], range_[1])
+                else:
+                    norm_ip(t, float(t.min()), float(t.max()))
+
+            if scale_each is True:
+                for t in tensor:  # loop over mini-batch dimension
+                    norm_range(t, range)
+            else:
+                norm_range(tensor, range)
+
+        if tensor.size(0) == 1:
+            return tensor.squeeze()
+
+        # make the mini-batch of images into a grid
+        nmaps = tensor.size(0)
+        xmaps = min(nrow, nmaps)
+        ymaps = int(math.ceil(float(nmaps) / xmaps))
+        height, width = int(tensor.size(2) + padding), int(tensor.size(3) + padding)
+        grid = tensor.new(3, height * ymaps + padding, width * xmaps + padding).fill_(pad_value)
+        k = 0
+        for y in range(ymaps):
+            for x in range(xmaps):
+                if k >= nmaps:
+                    break
+                grid.narrow(1, y * height + padding, height - padding)\
+                    .narrow(2, x * width + padding, width - padding)\
+                    .copy_(tensor[k])
+                k = k + 1
+        return grid
+
+    @staticmethod
+    def get_image_grid(tensor, filename, nrow=3, padding=2,mean=None, std=None):
+        """
+        Saves a given Tensor into an image file.
+        If given a mini-batch tensor, will save the tensor as a grid of images.
+        """
+        from PIL import Image
+        
+        # tensor = tensor.cpu()
+        grid = ObjectDetector.make_grid(tensor, nrow=nrow, padding=10,pad_value=1)
+        if not mean is None:
+            # ndarr = grid.mul(std).add(mean).mul(255).byte().transpose(0,2).transpose(0,1).numpy()
+            ndarr = grid.mul(std).add(mean).mul(255).byte().transpose(0,2).transpose(0,1).numpy()
+        else:      
+            ndarr = grid.mul(0.5).add(0.5).mul(255).byte().transpose(0,2).transpose(0,1).numpy()
+        im = Image.fromarray(ndarr)
+        # im.save(filename)
+        return im
+
 
     @staticmethod
     def find_object_poses(vertex2, aff, pnp_solver, config):
